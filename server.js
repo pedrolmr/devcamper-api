9303d7cb78f739b5236a2afd4287b2a302d3819d
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-06-01 23:24:04 -0400

    connected to mongodb database

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
+//connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mout routes
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1)); //the 1 exits with failure
+});
