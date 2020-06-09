9452c1b74ed10e90f6c3702089d3e29ae3400e41
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-06-09 00:19:56 -0400

    added course routes and controller

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,7 +9,12 @@ const {
   getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 
+//include other resource routers
+const courseRouter = require('./courses');
 const router = express.Router();
+
+//Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mout routes
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
